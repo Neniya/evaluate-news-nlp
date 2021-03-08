cf43417c154d025d4dc1e2845f668bea74752c8d
Evgeniya Kotova <zheniyasl@gmail.com>
2021-03-08 14:47:20 +0100

    add the babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,13 @@ const path = require("path");
 const webpack = require("webpack");
 module.exports = {
   entry: "./src/client/index.js",
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
 };
