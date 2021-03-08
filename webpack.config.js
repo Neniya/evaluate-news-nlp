d952a4a88807eb4660bde35ec228e381c494e421
Evgeniya Kotova <zheniyasl@gmail.com>
2021-03-08 15:10:22 +0100

    install the webpack server add clean webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
@@ -17,6 +18,15 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/client/views/index.html",
       filename: "./index.html",
+    }),
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
     }),
   ],
 };
