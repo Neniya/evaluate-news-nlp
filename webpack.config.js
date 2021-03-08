f6910ffbfa84450d4147812aebdc2fb987adb300
Evgeniya Kotova <zheniyasl@gmail.com>
2021-03-08 15:02:26 +0100

    add plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: "./src/client/index.js",
   module: {
@@ -11,4 +13,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+  ],
 };
