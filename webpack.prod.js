18209aecf7065cc3231eeaa09c1d3bdc0106a978
Evgeniya Kotova <zheniyasl@gmail.com>
2021-03-08 16:39:16 +0100

    install sass loaders for dev and prod mode

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,34 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+  entry: "./src/client/index.js",
+  mode: "production",
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+  ],
+};
