2c0b6f973651f481b319e5e53082ff3aea389a0d
Evgeniya Kotova <zheniyasl@gmail.com>
2021-03-10 15:08:08 +0100

    set server

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,31 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 var path = require("path");
 const express = require("express");
 const mockAPIResponse = require("./mockAPI.js");
 
 const app = express();
+const API_KEY = process.env.API_KEY;
 
 app.use(express.static("dist"));
 
-console.log(__dirname);
+//config express to use body-parser as middle-ware
+const bodyParser = require("body-parser");
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+//Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
+
+//console.log(__dirname);
 
 app.get("/", function (req, res) {
-  // res.sendFile('dist/index.html')
-  res.sendFile(path.resolve("dist/index.html"));
+  res.sendFile("dist/index.html");
+  //res.sendFile(path.resolve("dist/index.html"));
 });
 
 // designates what port the app will listen to for incoming requests
